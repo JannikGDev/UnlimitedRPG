69cff1c7fee55ba9f5b5cdaccafebcfc16960c62
Jannik G. <jannikgroeger@gmx.net>
2026-03-17 19:03:44 +0100

    API Setup

--- UnlimitedRPG.Api/Program.cs
+++ UnlimitedRPG.Api/Program.cs
@@ -2,11 +2,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddSignalR();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
     app.MapOpenApi();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapGet("/health", () => "ok").WithName("Health");
 
--- UnlimitedRPG.Core/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace RpgFramework.Core;
-
-public class Class1
-{
-
-}
